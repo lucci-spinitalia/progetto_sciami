c52165d9f7f56e474cd8c6dedc06bd332e22bb80
Luca Lucci <l.lucci@spinitalia.com>
2014-06-04 10:43:36 +0200

    - Creata la cartella a parte per il firmware

--- Debug/Rs232/rs232.d
+++
@@ -1,4 +0,0 @@
-Rs232/rs232.d: /home/luca/beaglebone_code/Common/Source/Rs232/rs232.c \
- /home/luca/beaglebone_code/Common/Include/rs232.h
-
-/home/luca/beaglebone_code/Common/Include/rs232.h:
--- Debug/main.d
+++
@@ -1,6 +0,0 @@
-main.d: ../main.c /home/luca/beaglebone_code/Common/Include/rs232.h \
- /home/luca/beaglebone_code/Common/Include/rs232.h
-
-/home/luca/beaglebone_code/Common/Include/rs232.h:
-
-/home/luca/beaglebone_code/Common/Include/rs232.h:
